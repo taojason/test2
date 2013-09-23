eb4845c4b463a32ef96c3e0e6857af581bcc6080
j23tao <taojason@gmail.com>
2013-09-22 23:58:17 -0700

    fix undefined method find_by_remember_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :password, :password_confirmation
+  attr_accessible :email, :name, :password, :password_confirmation, :remember_token
   has_secure_password
   has_many :microposts, dependent: :destroy
   has_many :jobs, dependent: :destroy
